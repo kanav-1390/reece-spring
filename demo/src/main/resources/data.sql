c1e00281b7070fa2594d100219a1b14b0c261f25
kanav-1390 <63841499+kanav-1390@users.noreply.github.com>
2023-10-02 00:49:52 +1100

    Update data.sql

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -2,7 +2,7 @@ INSERT INTO "contact"("name","email") VALUES ('KS','abc.gmail.com');
 INSERT INTO "contact"("name","email") VALUES ('AJ','def.gmail.com');
 INSERT INTO "contact"("name","email") VALUES ('Chai','ghi.gmail.com');
 
-INSERT INTO "address_book"("name","phone_number","contactid") VALUES ('KS','123456789',1);
-INSERT INTO "address_book"("name","phone_number","contactid")  VALUES ('AJ','987654321',2);
-INSERT INTO "address_book"("name","phone_number","contactid")  VALUES ('Chai','11111111',3);
-INSERT INTO "address_book" ("name","phone_number","contactid")  VALUES ('KS','1111111',1);
+INSERT INTO "address_book"("name","phone_number","contactid") VALUES ('KS','1234567890',1);
+INSERT INTO "address_book"("name","phone_number","contactid")  VALUES ('AJ','1234567890',2);
+INSERT INTO "address_book"("name","phone_number","contactid")  VALUES ('Chai','1234567894',3);
+INSERT INTO "address_book" ("name","phone_number","contactid")  VALUES ('KS','1234567897',1);
